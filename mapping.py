a6a3008bb11defbac5d9c26af445ce8ca96758e6
Sv0gth1r <youri.gicquel@gmail.com>
2026-07-16 22:26:21 +0200

    add request to fetch bars

---
+++ mapping.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+
+import requests
+
+NOMINATIM_CITY_SEARCH="https://nominatim.openstreetmap.org/search?format=json&q="
+OVERPASS_API="https://overpass-api.de/api/interpreter"
+SR_USER_AGENT={"User-Agent": "ShadowrunMapper/1.0"}
+
+@dataclass
+class BBox:
+    south: float
+    west: float
+    north: float
+    east: float
+
+#----------------------------------------------------
+# get_city_coords
+# [IN] city_name (string): Name of the city to find
+# [OUT] obj (City_coords): Two coordinates (x,y) to
+#   draw a square around the city
+#----------------------------------------------------
+def get_city_coords(city_name):
+    url = f"{NOMINATIM_CITY_SEARCH}{city_name}"
+    resp = requests.get(url, headers=SR_USER_AGENT)
+    data = resp.json()
+    if data:
+        # For simplicity here, we take only the first result
+        # TODO: allow user to choose if several cities share the same name
+        bbox=BBox(
+                float(data[0]['boundingbox'][0]),
+                float(data[0]['boundingbox'][1]),
+                float(data[0]['boundingbox'][2]),
+                float(data[0]['boundingbox'][3]))
+        return bbox 
+    else:
+        raise ValueError("City not found")
+
+#----------------------------------------------------
+# get_elements_in_square
+# [IN] coordinates (Coords)
+#----------------------------------------------------
+def get_elements_in_square(bbox: BBox):
+    query = f"""
+    [out:json][timeout:90];
+    (
+        node["bar"]({bbox.south},{bbox.west},{bbox.north},{bbox.east});
+    );
+    out body;
+    """
+    resp = requests.post(OVERPASS_API, data=query.encode(), headers=SR_USER_AGENT, timeout=240)
+    resp.raise_for_status()
+    return resp.json()
